d05f954d63381b10a5a874cb63bde629143e4447
TheStormThatIsApproaching <73610936+Baraha@users.noreply.github.com>
2021-08-15 16:02:00 +0300

    Перевел весь сервер на docker
    Перевел весь сервер на docker  и сделал подключение к mongo

--- go.mod
+++ go.mod
@@ -2,4 +2,16 @@ module github.com/Baraha/crypto_server.git
 
 go 1.16
 
-require github.com/fasthttp/router v1.4.1 // indirect
+require (
+	github.com/andybalholm/brotli v1.0.3 // indirect
+	github.com/fasthttp/router v1.4.1
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/klauspost/compress v1.13.4 // indirect
+	github.com/valyala/fasthttp v1.28.0
+	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
+	go.mongodb.org/mongo-driver v1.7.1
+	golang.org/x/crypto v0.0.0-20210813211128-0a44fdfbc16e // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
+)
--- main.go
+++ main.go
@@ -1,19 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 
 	"github.com/fasthttp/router"
 	"github.com/valyala/fasthttp"
-)
-
-const (
-	hosts      = "ds026491.mongolab.com:26491"
-	database   = "messagingdb"
-	username   = "admin"
-	password   = "youPassword"
-	collection = "messages"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func Index(ctx *fasthttp.RequestCtx) {
@@ -25,10 +20,26 @@ func Hello(ctx *fasthttp.RequestCtx) {
 }
 
 func main() {
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://mongo:27017/")
+
+	// Connect to MongoDB
+	Client, _ := mongo.Connect(context.TODO(), clientOptions)
+
+	// Check the connection
+	err := Client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
 	r := router.New()
 	r.GET("/", Index)
 	r.GET("/hello/{name}", Hello)
+	fmt.Printf("server starting")
 
 	log.Fatal(fasthttp.ListenAndServe(":8080", r.Handler))
+	fmt.Println("server is start!")
 
 }
